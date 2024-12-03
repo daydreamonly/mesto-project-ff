ccf314788e90fd63bbca3dc45b17188944effb2c
daydreamer <nimandrey22@gmail.com>
2024-12-03 19:02:41 +0300

    Исправление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -5,22 +5,21 @@ const cardTemplate = document.querySelector("#card-template").content;
 const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-function createCards(arr) {
-  arr.forEach((card) => {
-    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-    const cardImage = cardElement.querySelector(".card__image");
-    const cardTitle = cardElement.querySelector(".card__title");
-    const deleteButton = cardElement.querySelector(".card__delete-button");
+function createCard(item, { deleteCard }) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
-    cardImage.src = card.link;
-    cardImage.alt = card.name;
-    cardTitle.textContent = card.name;
-    deleteButton.addEventListener("click", () => {
-      deleteCard(cardElement);
-    });
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+  cardTitle.textContent = item.name;
 
-    return placesList.append(cardElement);
+  deleteButton.addEventListener("click", () => {
+    deleteCard(cardElement);
   });
+
+  return cardElement;
 }
 
 // @todo: Функция удаления карточки
@@ -29,4 +28,11 @@ function deleteCard(card) {
 }
 
 // @todo: Вывести карточки на страницу
-createCards(initialCards);
+function renderCards(arr) {
+  arr.forEach((card) => {
+    const cardElement = createCard(card, { deleteCard });
+    return placesList.append(cardElement);
+  });
+}
+
+renderCards(initialCards);
